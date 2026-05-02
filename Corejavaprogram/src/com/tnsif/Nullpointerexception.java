48a824a2c7b06f3a6b978e7b16d06db1f917011a
SANJANASPOOJARY <sanjanaspoojary44@gmail.com>
2026-05-02 08:01:18 +0530

    This is Exceptionhandling commit

---
+++ Corejavaprogram/src/com/tnsif/Nullpointerexception.java
@@ -0,0 +1,13 @@
+package com.tnsif.Exceptionhandling;
+
+public class Nullpointerexception {
+	public static void main(String[] args) {
+		String str=null;
+		
+		try {
+			System.out.println(str.length());
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
