3e82c5feece7b23bd912ee753593fdba49829b1a
SANJANASPOOJARY <sanjanaspoojary44@gmail.com>
2026-05-03 08:53:10 +0530

    This is JavaAbstract files

---
+++ Corejavaprogram/src/com/tnsif/MyTest.java
@@ -0,0 +1,12 @@
+package com.tnsif.Javaabtract;
+
+
+public abstract class MyTest {
+	
+	//abstract method--->it has no body
+	abstract void calculate();
+	
+	void display() {
+		System.out.println("Welcome to abstract class");
+	}
+}
